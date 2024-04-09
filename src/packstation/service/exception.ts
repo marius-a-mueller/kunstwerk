a9b5d550e542ff842a5d6c9a29cf261072e571fc
grso1012 <grimpe.soulaiman@gmail.com>
2024-04-09 10:51:33 +0200

    service

---
+++ src/packstation/service/exception.ts
@@ -0,0 +1,56 @@
+// eslint-disable-next-line max-classes-per-file
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+/**
+ * Das Modul besteht aus den Klassen für die Fehlerbehandlung bei der Verwaltung
+ * von Packstationen, z.B. beim DB-Zugriff.
+ * @packageDocumentation
+ */
+
+/**
+ * Exception-Klasse für eine ungültige Packstations-ID.
+ */
+export class PackstationIdInvalidException extends HttpException {
+    constructor(readonly id: number | undefined) {
+        super(
+            `Die Packstations-ID ${id} ist ungültig oder nicht vorhanden.`,
+            HttpStatus.NOT_FOUND,
+        );
+    }
+}
+
+/**
+ * Exception-Klasse für eine nicht gefundene Packstation.
+ */
+export class PackstationNotFoundException extends HttpException {
+    constructor(readonly id: number | undefined) {
+        super(
+            `Die Packstation mit der ID ${id} wurde nicht gefunden.`,
+            HttpStatus.NOT_FOUND,
+        );
+    }
+}
+
+/**
+ * Exception-Klasse für eine ungültige Versionsnummer beim Ändern.
+ */
+export class VersionInvalidException extends HttpException {
+    constructor(readonly version: string | undefined) {
+        super(
+            `Die Versionsnummer ${version} ist ungueltig.`,
+            HttpStatus.PRECONDITION_FAILED,
+        );
+    }
+}
+
+/**
+ * Exception-Klasse für eine veraltete Versionsnummer beim Ändern.
+ */
+export class VersionOutdatedException extends HttpException {
+    constructor(readonly version: number) {
+        super(
+            `Die Versionsnummer ${version} ist nicht aktuell.`,
+            HttpStatus.PRECONDITION_FAILED,
+        );
+    }
+}
---
+++ src/packstation/service/packstation-write.service.ts
@@ -0,0 +1,189 @@
+/**
+ * Das Modul besteht aus der Klasse {@linkcode PackstationWriteService} für die
+ * Schreiboperationen im Anwendungskern.
+ * @packageDocumentation
+ */
+import { InjectRepository } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { MailService } from '../../mail/mail.service';
+// eslint-disable-next-line sort-imports
+import { type DeleteResult, Repository } from 'typeorm';
+import { Adresse } from '../entity/adresse.entity';
+import { Packstation } from '../entity/packstation.entity';
+import { PackstationReadService } from './packstation-read.service';
+import { Paket } from '../entity/paket.entity';
+import { getLogger } from '../../logger/logger';
+// eslint-disable-next-line sort-imports
+import {
+    PackstationIdInvalidException,
+    PackstationNotFoundException,
+    VersionInvalidException,
+    VersionOutdatedException,
+} from './exception';
+
+export interface UpdateParams {
+    readonly id: number | undefined;
+    readonly packstation: Packstation;
+    readonly version: string;
+}
+
+@Injectable()
+export class PackstationWriteService {
+    private static readonly VERSION_PATTERN = /^"\d{1,3}"/u;
+
+    readonly #repo: Repository<Packstation>;
+    readonly #readService: PackstationReadService;
+    readonly #mailService: MailService;
+    readonly #logger = getLogger(PackstationWriteService.name);
+
+    constructor(
+        @InjectRepository(Packstation)
+        repo: Repository<Packstation>,
+        readService: PackstationReadService,
+        mailService: MailService,
+    ) {
+        this.#repo = repo;
+        this.#readService = readService;
+        this.#mailService = mailService;
+    }
+
+    /**
+     * Erstellt eine neue Packstation.
+     * @param packstation Die Packstation, die erstellt werden soll.
+     * @returns Die erstellte Packstation.
+     */
+    async create(packstation: Packstation): Promise<number> {
+        this.#logger.debug('create: packstation=%o', packstation);
+        await this.#validateCreate(packstation);
+
+        const packstationDb = await this.#repo.save(packstation);
+        this.#logger.debug('create: packstationDb=%o', packstationDb);
+
+        await this.#sendmail(packstationDb);
+
+        return packstationDb.id!;
+    }
+
+    /**
+     * Aktualisiert eine Packstation.
+     * @param params Die Parameter für die Aktualisierung.
+     * @returns Die aktualisierte Packstation.
+     */
+    async update({ id, packstation, version }: UpdateParams): Promise<number> {
+        this.#logger.debug(
+            'update: id=%d, packstation=%o, version=%s',
+            id,
+            packstation,
+            version,
+        );
+        if (id === undefined) {
+            this.#logger.debug('update: Keine gueltige ID');
+            throw new PackstationNotFoundException(id);
+        }
+
+        const validateResult = await this.#validateUpdate(
+            packstation,
+            id,
+            version,
+        );
+        this.#logger.debug('update: validateResult=%o', validateResult);
+        if (!(validateResult instanceof Packstation)) {
+            return validateResult;
+        }
+
+        const packstationNeu: Packstation = validateResult;
+        const merged: Packstation = this.#repo.merge(
+            packstation,
+            packstationNeu,
+        );
+        this.#logger.debug('update: merged=%o', merged);
+        const updated: Packstation = await this.#repo.save(merged);
+        this.#logger.debug('update: updated=%o', updated);
+
+        return updated.version!;
+    }
+
+    /**
+     * Loescht eine Packstation.
+     * @param id Die ID der Packstation, die geloescht werden soll.
+     */
+    async delete(id: number): Promise<boolean> {
+        this.#logger.debug('delete: id=%d', id);
+        const packstation = await this.#readService.findById({ id });
+
+        let deleteResult: DeleteResult | undefined;
+        await this.#repo.manager.transaction(async (transactionalMgr) => {
+            // Adresse löschen
+            const adresseId = packstation.adresse?.id;
+            if (adresseId !== undefined) {
+                await transactionalMgr.delete(Adresse, adresseId);
+            }
+
+            // Pakete löschen
+            const pakete = packstation.pakete ?? [];
+            for (const paket of pakete) {
+                await transactionalMgr.delete(Paket, paket.id);
+            }
+
+            // Packstation löschen
+            deleteResult = await transactionalMgr.delete(Packstation, id);
+            this.#logger.debug('delete: deleteResult=%o', deleteResult);
+        });
+
+        return (
+            deleteResult?.affected !== undefined &&
+            deleteResult.affected !== null &&
+            deleteResult.affected > 0
+        );
+    }
+
+    async #validateCreate({ id }: Packstation): Promise<undefined> {
+        this.#logger.debug('#validateCreate: baudatum=%s', id);
+        if (await this.#repo.existsBy({ id })) {
+            throw new PackstationIdInvalidException(id);
+        }
+    }
+
+    async #sendmail(packstation: Packstation) {
+        const subject = `Neue Packstation ${packstation.id}`;
+        const { nummer } = packstation;
+        const body = `Die Packstation mit der Nummer <strong>${nummer}</strong> ist angelegt worden.`;
+        await this.#mailService.sendmail({ subject, body });
+    }
+
+    async #validateUpdate(
+        packstation: Packstation,
+        id: number,
+        versionStr: string,
+    ): Promise<Packstation> {
+        this.#logger.debug(
+            '#validateUpdate: packstation=%o, id=%d, versionStr=%s',
+            packstation,
+            id,
+            versionStr,
+        );
+        if (!PackstationWriteService.VERSION_PATTERN.test(versionStr)) {
+            throw new VersionInvalidException(versionStr);
+        }
+
+        const version = Number.parseInt(versionStr.slice(1, -1), 10);
+        this.#logger.debug(
+            '#validateUpdate: packstation=%o, version=%d',
+            packstation,
+            version,
+        );
+
+        const packstationDb = await this.#readService.findById({ id });
+
+        const versionDb = packstationDb.version!;
+        if (version < versionDb) {
+            this.#logger.debug('#validateUpdate: versionDb=%d', versionDb);
+            throw new VersionOutdatedException(version);
+        }
+        this.#logger.debug(
+            '#validateUpdatePackstation: packstationDb=%o',
+            packstationDb,
+        );
+        return packstationDb;
+    }
+}
