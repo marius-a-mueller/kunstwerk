a4b82c7ffb892b7724d7ebfacd867bb6d75b1876
Marius Müller <marius@bscnet.de>
2024-04-09 14:03:36 +0200

    SQL Statements hinzugefügt. Typen angepasst.

--- src/config/resources/db/postgres/create.sql
+++ src/config/resources/db/postgres/create.sql
@@ -13,4 +13,30 @@
 -- "user-private schema" (Default-Schema: public)
 CREATE SCHEMA IF NOT EXISTS AUTHORIZATION packstation;
 
--- TODO SQL Inserts erstellen!
+ALTER ROLE packstation SET search_path = 'packstation';
+
+CREATE TABLE IF NOT EXISTS packstation (
+    id                integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE packstationspace,
+    version           integer NOT NULL DEFAULT 0,
+    nummer            varchar(30) NOT NULL UNIQUE USING INDEX TABLESPACE packstationspace,
+    baudatum          date,
+    erzeugt           timestamp NOT NULL DEFAULT NOW(),
+    aktualisiert      timestamp NOT NULL DEFAULT NOW()
+) TABLESPACE packstationspace;
+
+CREATE TABLE IF NOT EXISTS adresse (
+    id                integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE packstationspace,
+    strasse           varchar(32) NOT NULL,
+    hausnummer        varchar(8) NOT NULL,
+    postleitzahl      varchar(5) NOT NULL,
+    stadt             varchar(32) NOT NULL,
+    packstation_id    integer NOT NULL UNIQUE USING INDEX TABLESPACE packstationspace REFERENCES packstation
+) TABLESPACE packstationspace;
+
+CREATE TABLE IF NOT EXISTS paket (
+    id                integer GENERATED ALWAYS AS IDENTITY(START WITH 1000) PRIMARY KEY USING INDEX TABLESPACE packstationspace,
+    nummer            varchar(30) NOT NULL UNIQUE,
+    max_gewicht_in_kg decimal(8,2),
+    packstation_id    integer NOT NULL REFERENCES packstation
+) TABLESPACE packstationspace;
+CREATE INDEX IF NOT EXISTS pakete_packstation_id_idx ON pakete(packstation_id) TABLESPACE packstationspace;
